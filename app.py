25ff60c4c2b7f2f9a08e02e0b4aed5f9036d04fd
John@Doe.org <sinnan.suleman@daraz.pk>
2022-06-16 20:35:18 +0500

    Logging

--- app.py
+++ app.py
@@ -16,7 +16,17 @@ rand="".join(random.choices(string.ascii_uppercase + string.digits, k=5))
 @app.route('/api/user/SomeRoute', methods=['DELETE'])
 def SomeFunctionality():
 	response={}
-	return "<h1>user service %%s</H1>"%%(rand)
+	logger(request,response)
+	return "<h1>user service %s</H1>"%(rand)
+
+def logger(request,response):
+	tmp='headers:{'
+	for header in request.headers:
+		tmp=tmp+'"'+header[0]+'":"'+header[1]+'"'
+	tmp=tmp+'}'
+	p=open(config["log_path"],"a")
+	p.write("[%s] %s %s headers:{%s} data:{%s}\n"%%(datetimex(datetime.datetime.now()),request.method,request.full_path,tmp,request.get_data(as_text=True)))
+	p.close()
 
 if __name__ == "__main__":
 	app.run(host=config["host"],port=config["port"])
